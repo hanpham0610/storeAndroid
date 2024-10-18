6d3548749b0e77803ecb9b257a856bda4adf8ad0
unknown <vienchelsea.it@gmail.com>
2024-10-18 20:38:40 +0700

    may cty

---
+++ app/src/main/java/com/example/store/Controller/SanPham/QuanLy/UpdateProductController.java
@@ -0,0 +1,16 @@
+package com.example.store.Controller.SanPham.QuanLy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.store.R;
+
+public class UpdateProductController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_product_controller);
+    }
+}
